c21a45db7bd71252ff176ce0009973d3df457067
RD <r.deans@outlook.com>
2022-12-01 23:37:15 -0500

    Added:
    - initial layout/setup

--- vis/teamInfo.php
+++ vis/teamInfo.php
@@ -1,17 +1,44 @@
 <?php
+
+session_start();
+
+require_once "signup.php";
+
+
+
+if(isset($_POST["submit"])){
+
+    $usr = $_SESSION['newUser'];
+    $pw = $_SESSION['newUserPassword'];
     $team = $_POST["team"];
+    $location= $_POST["location"];
     $division = $_POST["division"];
-    $coach = $_POST["coach"];
+    $coachFname = $_POST["coach_first_name"];
+    $coachLname = $_POST["coach_last_name"];
     $coachPay = $_POST["coach_pay"];
     $coachExp = $_POST["coach_num_seasons"];
-    if(addUser($_SESSION['newUser'],$_SESSION['newUserPassword'],$team, $division, $coach, $coachPay, $coachExp)){
 
+    if(hasEmpty($usr, $pw, $team, $division, $location, $coachFname, $coachLname, $coachPay, $coachExp)){
+        alert("one or more fields are empty");
     }
     else{
-        alert("Something went wrong");
-        header("location: index.php");
-        exit();
+        $registration = new registration();
+
+        if($registration->registerNew($usr, $pw, $team, $division, $location,
+            $coachFname, $coachLname, $coachExp, $coachPay)){
+            header("location: user_home.php");
+            exit();
+        }
+        else{
+            alert("Something went wrong");
+            header("location: index.php");
+            exit();
+        }
+
     }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +47,49 @@
     <title>info</title>
 </head>
 <body>
+    <h2>
+        Team Information
+    </h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <label>
+            <input name="team" placeholder="team name">
+        </label>
 
+        <label>
+            <input name="location" placeholder="state">
+        </label>
+
+        <label>
+            <select name="division">
+                <option value="">division</option>
+                <option value="North">North</option>
+                <option value="East">East</option>
+                <option value="South">South</option>
+                <option value="West">West</option>
+            </select>
+        </label>
+
+        <label>
+            <input name="coach_first_name" placeholder="coach first name">
+        </label>
+
+        <label>
+            <input name="coach_last_name" placeholder="coach last name">
+        </label>
+
+        <label>$</label>
+        <label>
+            <input name="coach_pay" placeholder="coach pay" type="number">
+        </label>
+        <label>.00</label>
+
+        <label>
+            <input name="coach_num_seasons" placeholder="number of seasons coached" type="number">
+        </label>
+        <label>seasons</label>
+
+        <button name="submit" >submit</button>
+
+    </form>
 </body>
 </html>
